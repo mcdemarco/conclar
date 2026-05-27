91132afd5d805ed7287c3cb109a71db06dc837a0
M. C. DeMarco <mcd@mcdemarco.net>
2026-05-27 12:13:53 -0400

    Pop out recording links and tag them

--- src/components/ItemLink.js
+++ src/components/ItemLink.js
@@ -1,7 +1,7 @@
 
 const ItemLink = ({ name, link, text, enabled }) => {
   return <div className={name}>
-    <a className={enabled ? null : "disabled"} href={enabled ? link : null}>{text}</a>
+    <a className={enabled ? null : "disabled"} href={enabled ? link : null} target="_blank">{text}</a>
   </div>;
 };
 
